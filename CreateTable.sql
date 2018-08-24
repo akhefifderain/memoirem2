8bbc6c2caa5ec1503c0b88af2f572e18fcf4612d
aurelienkd <aurelien.khefifderain@gmail.com>
2018-08-25 00:59:46 +0200

    Add SQL Redshift Create Table

---
+++ CreateTable.sql
@@ -0,0 +1,18 @@
+create SCHEMA memoire_sc;
+create table memoire_sc.pageview
+(
+  "adresse_ip_user" varchar(264),
+"id_session" varchar(264),
+  "date_action" varchar(264),
+  "type_device" varchar(264),
+  "navigateur" varchar(264),
+  "OS" varchar(264),
+  "event_name" varchar(264),
+  "merchant_id" varchar(264),
+  "merchant_name" varchar(264),
+  "category_id" varchar(264),
+  "id_offer" varchar(264),
+  "value_offer" varchar(264),
+  "typer_offer" varchar(264),
+  "NumberPVSession" varchar(264)
+)
